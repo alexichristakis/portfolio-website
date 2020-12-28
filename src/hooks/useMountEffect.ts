42b7e5ef99e16f3d83d3627639d9f52780ce8300
Alexi Christakis <alexi.christakis@gmail.com>
2020-12-27 23:37:33 -0800

    some improvements

--- src/hooks/useMountEffect.ts
+++ src/hooks/useMountEffect.ts
@@ -1,6 +1,11 @@
-import { useEffect, EffectCallback } from "react";
+import { useEffect, useLayoutEffect, EffectCallback } from "react";
 
 export const useMountEffect = (effect: EffectCallback) => {
   // eslint-disable-next-line
   useEffect(effect, []);
+};
+
+export const useLayoutMountEffect = (effect: EffectCallback) => {
+  // eslint-disable-next-line
+  useLayoutEffect(effect, []);
 };
